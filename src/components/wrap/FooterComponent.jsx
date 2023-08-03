b53a7a01f96ec004fd6033d88f678c0b5227d548
qkrguswls <qkrguswls309@naver.com>
2023-08-03 19:09:01 +0900

    브란도 포토그라피 리액트 CRA git배포

---
+++ src/components/wrap/FooterComponent.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function FooterComponent() {
+   return (
+      <footer id="footer">
+            <div className="container">
+               <div className="gap">
+                  <div className="row1">
+                        <a href="!#"><i className="fa fa-facebook-f"></i></a>
+                        <a href="!#"><i className="fa fa-twitter"></i></a>
+                        <a href="!#"><i className="fa fa-dribbble"></i></a>
+                        <a href="!#"><i className="fa fa-youtube-play"></i></a>
+                        <a href="!#"><i className="fa fa-linkedin"></i></a>
+                  </div>
+                  <hr/>
+                  <div className="row2">
+                     <p>© 2022 BRANDO IS PROUDLY POWERED BY THEMEZAA.</p>
+                  </div>
+               </div>
+            </div>
+      </footer>
+   );
+};
+
